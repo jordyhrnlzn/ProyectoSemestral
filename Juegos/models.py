b50010d25527986f5d9a8e1878935730b2af2e31
jordyhrnlzn <jo.lizana@duocuc.cl>
2022-05-20 19:47:08 -0400

    detalles

--- Juegos/models.py
+++ Juegos/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    IdCategoria = models.AutoField(primary_key=True, verbose_name='ID autoincrementable de la categori')
+    nombreCategoria = models.CharField(max_length=20, verbose_name='Nombre de la categoria', blank=False, null=False)
+
+    def __str__(self):
+        return self.nombreVideojuego
+
 class Videojuego(models.Model):
-    IdJuego = models.AutoField(primary_key=True, verbose_name='ID autoincrementable del juego')
     nombreVideojuego = models.CharField(max_length=20, verbose_name='Nombre del videojuego', blank=False, null=False)
     AñoLanzamiento = models.IntegerField(verbose_name='Año de salida del videojuego')
     imagen = models.ImageField(upload_to='videojuegos')
