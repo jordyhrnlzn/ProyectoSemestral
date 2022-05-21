ae116866ba8a03159c57bd0cbe73bffdcdf02e7f
jordyhrnlzn <jo.lizana@duocuc.cl>
2022-05-21 14:23:13 -0400

    arreglo de los modelos

--- Juegos/admin.py
+++ Juegos/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from.models import Videojuego
+from.models import Categoria
 # Register your models here.
 admin.site.register(Videojuego)
+admin.site.register(Categoria)
--- Juegos/models.py
+++ Juegos/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Categoria(models.Model):
-    IdCategoria = models.AutoField(primary_key=True, verbose_name='ID autoincrementable de la categori')
+    IdCategoria = models.AutoField(primary_key=True, verbose_name='ID autoincrementable de la categoria')
     nombreCategoria = models.CharField(max_length=20, verbose_name='Nombre de la categoria', blank=False, null=False)
 
     def __str__(self):
-        return self.nombreVideojuego
+        return self.nombreCategoria
 
 class Videojuego(models.Model):
+    IdJuego = models.AutoField(primary_key=True, verbose_name='ID autoincrementable del juego')
     nombreVideojuego = models.CharField(max_length=20, verbose_name='Nombre del videojuego', blank=False, null=False)
     AñoLanzamiento = models.IntegerField(verbose_name='Año de salida del videojuego')
     imagen = models.ImageField(upload_to='videojuegos')
+    categoria = models.ForeignKey(Categoria,on_delete= models.CASCADE)
 
     def __str__(self):
         return self.nombreVideojuego
